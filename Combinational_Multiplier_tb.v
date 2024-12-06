9706c98666595471ba5d31750ca9cf509be44f02
Edwin Chung <144295747+master-edwon@users.noreply.github.com>
2024-12-06 14:58:00 -0500

    Update Combinational_Multiplier_tb.v

--- Combinational_Multiplier_tb.v
+++ Combinational_Multiplier_tb.v
@@ -5,27 +5,23 @@ module Combinational_Multiplier_tb();
     reg [3:0] a, b;
     wire [7:0] product;
 
-    // Instantiate the multiplier  
     Combinational_Multiplier uut (a,b,product);
     
-    // Test sequence
     initial begin
-        // Test case 1: Multiply 3 * 2
-        a = 4'b0011; // 3
-        b = 4'b0010; // 2
+        // 3 * 2
+        a = 4'b0011; 
+        b = 4'b0010; 
         #10;
 
-        // Test case 2: Multiply 7 * 4
-        a = 4'b0111; // 7
-        b = 4'b0100; // 4
-        #10;
-        $display("Test 2: %d * %d = %d", a, b, product);
+        // Multiply 7 * 4
+        a = 4'b0111; 
+        b = 4'b0100; 
+        #10
+        // 15 * 15
+        a = 4'b1111; 
+        b = 4'b1111; 
 
-        // Test case 3: Multiply 15 * 15
-        a = 4'b1111; // 15
-        b = 4'b1111; // 15
-
-        #10 $finish; // End simulation
+        #10 $finish;
     end
 endmodule
 
